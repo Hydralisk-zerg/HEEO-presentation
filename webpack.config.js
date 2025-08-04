ea6898102fd9c0b02b07edc1e7ae39310ad40488
Aleks <aleksey.ilchenko.m@gmail.com>
2025-08-04 15:38:39 +0300

    Удален старый index.html и vite.config.js - исправлены 404 ошибки для src/main.jsx

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/[name].[contenthash].js',
+    clean: true,
+    publicPath: '/HEEO-presentation/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'HEEO 2025 - Hellmann East Europe Overseas'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/images',
+          to: 'images'
+        }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true
+  }
+};
